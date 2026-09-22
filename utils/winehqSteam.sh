b2208d2303265c65becd9571e70c73064eb2ce3e
Viacheslav <celesik@gmail.com>
2026-09-22 14:31:57 +0700

    fix: small fixes for wine

--- utils/winehqSteam.sh
+++ utils/winehqSteam.sh
@@ -16,37 +16,16 @@ fi
 echo "The OS release is: $release"
 echo "The OS version is: $codename"
 
-sudo mkdir -pm755 /etc/apt/keyrings
-wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
-sudo dpkg --add-architecture i386
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/$release/dists/$codename/winehq-$codename.sources
-
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y libpoppler-glib8:{i386,amd64}
-# wine winecfg
-sudo apt install --install-recommends winehq-stable -y
-
-sudo apt install -y \
-  librust-proton-call-dev proton-caller
+sudo apt install -y wine64 wine32 libvulkan1 libvulkan1:i386 \
+  libpoppler-glib8:{i386,amd64} librust-proton-call-dev proton-caller
   # libgl1-mesa-glx:{i386,amd64}
+
 sudo apt install --fix-broken -y
 sudo apt install -y meson gcc-mingw-w64-i686 g++-mingw-w64-i686 \
   g++-mingw-w64-x86-64-posix g++-mingw-w64-x86-64-win32 glslang-tools
-
-cd ~
-git clone --recursive https://github.com/HansKristian-Work/vkd3d-proton
-cd vkd3d-proton
-chmod +x ./package-release.sh
-mkdir -p $HOME/vkd3d
-sudo ./package-release.sh master $HOME/vkd3d --no-package
-cd ~
-sudo rm -rf vkd3d-proton
-
-# change to build.86 for 32-bit
-sudo chmod +x $HOME/vkd3d/vkd3d-proton-master/setup_vkd3d_proton.sh
-$HOME/vkd3d/vkd3d-proton-master/setup_vkd3d_proton.sh install
 
 # wget -O $HOME/steam.deb http://media.steampowered.com/client/installer/steam.deb
 wget https://repo.steampowered.com/steam/archive/precise/steam_latest.deb
@@ -61,6 +40,26 @@ tee -a $HOME/.steam/steam/steam_dev.cfg <<< \
 flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
 sudo flatpak override com.usebottles.bottles --filesystem=$HOME/.local/share/Steam
 
+
+# sudo mkdir -pm755 /etc/apt/keyrings
+# wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
+# sudo dpkg --add-architecture i386
+# sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/$release/dists/$codename/winehq-$codename.sources
+# wine winecfg
+# sudo apt install --install-recommends winehq-stable -y
+
+# cd ~
+# git clone --recursive https://github.com/HansKristian-Work/vkd3d-proton
+# cd vkd3d-proton
+# chmod +x ./package-release.sh
+# mkdir -p $HOME/vkd3d
+# sudo ./package-release.sh master $HOME/vkd3d --no-package
+# cd ~
+# sudo rm -rf vkd3d-proton
+
+# # change to build.86 for 32-bit
+# sudo chmod +x $HOME/vkd3d/vkd3d-proton-master/setup_vkd3d_proton.sh
+# $HOME/vkd3d/vkd3d-proton-master/setup_vkd3d_proton.sh install
 
 # primerun %command% -input_button_code_is_scan_code -vulkan_disable_steam_shader_cache
 # gamemoderun mangohud __NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia %command%
