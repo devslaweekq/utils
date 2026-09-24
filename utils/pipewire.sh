d48a649feff1e1222991d0ce7383eca4ed12dc37
Viacheslav <celesik@gmail.com>
2026-09-24 22:33:27 +0700

    fix: small fixes for paths

--- utils/pipewire.sh
+++ utils/pipewire.sh
@@ -5,5 +5,5 @@ sudo apt install -y pulseaudio-utils pavucontrol \
   pipewire-alsa pipewire-jack pipewire-audio-client-libraries
 
 mkdir -p ~/.config/pipewire
-cp /mnt/d/CRYPTO/utils/utils/pipewire.conf ~/.config/pipewire
+curl -fsSL https://raw.githubusercontent.com/devslaweekq/utils/main/utils/pipewire.conf -o ~/.config/pipewire/pipewire.conf
 systemctl --user restart pipewire wireplumber pipewire-pulse
